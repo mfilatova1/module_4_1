313fff408f94c1a5ef6e98d7e90e93ae5b8bb63a
mfilatova1 <filatovam435@gmail.com>
2024-07-02 20:04:20 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(first, second):
+    if second > 0:
+        a = first / second
+        print(a)
+    elif second == 0:
+        print('Ошибка')
+
+
+
---
+++ true_math.py
@@ -0,0 +1,12 @@
+from math import inf
+
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    elif second > 0:
+        a = first / second
+        print(a)
+
+
+
